7e318b30770556b7378429681ef8ad343081b677
coco025 <31966704+coco025@users.noreply.github.com>
2019-06-05 17:52:49 -0600

    Update makeRequest.js

--- SegmentTrackerAPP/templates/SegmentTracker_WEB/assets/js/makeRequest.js
+++ SegmentTrackerAPP/templates/SegmentTracker_WEB/assets/js/makeRequest.js
@@ -3,7 +3,7 @@ var xmlHttpRequest = new XMLHttpRequest();
 function makeRequest(query){
     query = "/SegmentTrackerAPP/" + query 
     //alert("El query: " + query);
-    xmlHttpRequest.open( "GET", query, true ); // false for synchronous request
+    xmlHttpRequest.open( "GET", query, false ); // false for synchronous request
 	xmlHttpRequest.send();
 }
 
